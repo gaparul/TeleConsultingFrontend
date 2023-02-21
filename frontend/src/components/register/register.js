40a1ea787db9248f3cc8e753fb906d964a21202f
sakshi-s0ni <sakshisoni61000@gmail.com>
2023-02-21 11:54:35 +0530

    Components

---
+++ frontend/src/components/register/register.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Navb from "../navbar/navbar";
+import Logo from "../login/logo";
+import regimg from "./temp.png";
+import "./regstyle.css";
+const Register = ()=>{
+return(
+    <>
+        <Logo/>
+        <Navb/>
+        <img src={regimg} alt="Image" className="reg-img"/>
+        <div className="card-container">
+            <div className="reg-card">
+                <div className="login-type"><h5>Patient Register</h5></div>
+                <div className="login-form">
+                    <form>
+                    <input type="text" id="patient-name" name="ptname" placeholder="Name" className="form-ele"/><br/>
+                    <input type="email" id="patient-email" name="ptemail" placeholder="E-mail" className="form-ele"/><br/>
+                    <input type="number" id="patient-number" name="ptnumber" placeholder="Mobile No." className="form-ele"/><br/>
+                    <input type="date" id="patient-dob" name="ptdob" placeholder="DoB" className="form-ele"/><br/>
+                    <input type="text" id="gender" name="ptgender" placeholder="Gender" className="form-ele"/>
+                    </form>
+                </div>
+                <Button variant="success" className="submit">Register</Button>
+            </div>
+        </div>
+    </>
+);
+};
+export default Register;
