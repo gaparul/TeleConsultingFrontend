1403188aab02b0d747b1f1dde7ba22acecc1bb9c
sakshi-s0ni <sakshisoni61000@gmail.com>
2023-02-25 20:56:33 +0530

    Doctor bean

---
+++ backend/teleconsulting/src/main/java/com/had/teleconsulting/teleconsulting/Payloads/DoctorDTO.java
@@ -0,0 +1,20 @@
+package com.had.teleconsulting.teleconsulting.Payloads;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class DoctorDTO {
+
+    int doctorID;
+    String doctorFirstName;
+    String doctorLastName;
+    String doctorPassword;
+    String doctorSpecialisation;
+}
