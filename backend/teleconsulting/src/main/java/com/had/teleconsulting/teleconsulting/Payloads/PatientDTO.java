ae8dadade9abe6b91e1d436e3db7186961bd65bf
Rupen-project <rupenrakholiya559@gmail.com>
2023-03-15 21:03:28 +0530

    DTO files is added

--- backend/teleconsulting/src/main/java/com/had/teleconsulting/teleconsulting/Payloads/PatientDTO.java
+++ backend/teleconsulting/src/main/java/com/had/teleconsulting/teleconsulting/Payloads/PatientDTO.java
@@ -4,18 +4,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @NoArgsConstructor
 @Getter
 @Setter
 
 public class PatientDTO {
-    private int patientID;
-    private String patientFirstName;
-    private String patientLastName;
-    private String patientMobileNumber;
-    private String patientEmail;
-    private String patientDOB;
-    private String patientGender;
-    private String patientPassword;
+
+    int patientID;
+    String patientFirstName;
+    String patientLastName;
+    String  patientMobileNumber;
+    String patientEmail;
+    String patientDOB;
+    String patientGender;
+    String patientPassword;
 
 }
