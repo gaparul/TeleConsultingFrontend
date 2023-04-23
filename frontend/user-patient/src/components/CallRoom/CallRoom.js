5a77c2ab04d6bde627ddea6169982972ac87cafe
Srivastava-Rani-Aakanksha <rani.aakanksha1999@gmail.com>
2023-04-23 15:19:34 +0530

    Minor changes

--- frontend/user-patient/src/components/CallRoom/CallRoom.js
+++ frontend/user-patient/src/components/CallRoom/CallRoom.js
@@ -1,18 +1,19 @@
 import React, { useState } from "react";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
 import { useParams } from "react-router-dom";
-import { Button} from "@mui/material";
-
+import { useNavigate } from 'react-router-dom';
 
 const CallRoom = () => {
     const roomId="1234";
+    const patientID = 6;
+    const navigate=useNavigate();
+    
     const myMeeting=async(element)=> {
         const appID = 868852693 ;
         const serverSecret = "9bf3442d9f083ba5f04468215a647d27";
         const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(
             appID, 
             serverSecret, 
-            //doctor id from backend will become the roomid
             roomId,  
             Date.now().toString(),
             "Patient"
@@ -28,41 +29,41 @@ const CallRoom = () => {
           showRoomTimer: true, 
           turnOnMicrophoneWhenJoining: false,
           turnOnCameraWhenJoining: false,
-        })    
-    }
+          showLeavingView: false,
+          onLeaveRoom: (async()=>{
+            try {
+                const response = await fetch(`http://localhost:8083/api/patientDetails/prescription/${patientID}`);
+                const blob = await response.blob();
+                // Handle the data returned from the API
+                const url = URL.createObjectURL(blob);
+                const link = document.createElement('a');
+                link.href = url;
+                link.download = 'prescription.pdf';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+                console.log('API response:', response.data);
+                if (response.data === 'Prescription Uploaded successfully') {
+                  alert('Download fail !');
+                }
+                else{
+                    alert('Prescription downloaded !'); 
+                }
+            } catch (error) {
+                // Handle errors
+                console.error(error);
+            }
 
-    const patientID = 6; //this will be taken from local storage 
-    const handleButtonClick = async() =>{
-        try {
-            const response = await fetch(`http://localhost:8083/api/patientDetails/prescription/${patientID}`);
-            const blob = await response.blob();
-            // Handle the data returned from the API
-            const url = URL.createObjectURL(blob);
-            const link = document.createElement('a');
-            link.href = url;
-            link.download = 'prescription.pdf';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        } catch (error) {
-            // Handle errors
-            console.error(error);
-        }
+            navigate('/')
+            window.location.reload('false');
+         })
+        })    
     }
 
     return (
         <>
             <div ref={myMeeting} 
                 style={{ width: '100vw', height: '80vh' }}>
-            </div>
-            <div style={{ textAlign: 'center' }}> 
-            <Button type="submit"
-                    variant="contained"
-                    size='medium'
-                    sx={{ mt: 1 , backgroundColor: '#1565c0' ,color:'white'}}
-                    onClick={handleButtonClick} >
-                Download prescription
-            </Button>
             </div>
         </>
     )
