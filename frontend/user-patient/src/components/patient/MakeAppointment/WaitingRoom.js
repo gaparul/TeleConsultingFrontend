8ebb59a81d319d138ddf1be3b3e509536b2212a4
Parul <parulghotikar@gmail.com>
2023-04-24 22:43:02 +0530

    formatted

--- frontend/user-patient/src/components/patient/MakeAppointment/WaitingRoom.js
+++ frontend/user-patient/src/components/patient/MakeAppointment/WaitingRoom.js
@@ -1,32 +1,32 @@
-import React from 'react'
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const card = (
-    <React.Fragment>
-      <CardContent>
-        <Typography sx={{ fontSize: 40 }} color="text.secondary" gutterBottom>
-          Waiting Queue...
-        </Typography>
-      </CardContent>
-      <CardActions>
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 40 }} color="text.secondary" gutterBottom>
+        Waiting Queue...
+      </Typography>
+    </CardContent>
+    <CardActions>
       <Button variant="contained" disabled>
-  Join Call
-</Button>
-      </CardActions>
-    </React.Fragment>
-  );
+        Join Call
+      </Button>
+    </CardActions>
+  </React.Fragment>
+);
 
 const WaitingRoom = () => {
   return (
-    <Box sx={{ minWidth: 300}}>
+    <Box sx={{ minWidth: 300 }}>
       <Card variant="outlined">{card}</Card>
     </Box>
-  )
-}
+  );
+};
 
-export default WaitingRoom
+export default WaitingRoom;
