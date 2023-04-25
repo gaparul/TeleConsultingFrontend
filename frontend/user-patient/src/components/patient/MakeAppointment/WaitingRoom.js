f8c463b91e33e39b2c56f99a266491b86331607e
sakshi-s0ni <sakshisoni61000@gmail.com>
2023-04-26 00:39:10 +0530

    Waiting Queue for patient

--- frontend/user-patient/src/components/patient/MakeAppointment/WaitingRoom.js
+++ frontend/user-patient/src/components/patient/MakeAppointment/WaitingRoom.js
@@ -16,6 +16,9 @@ const card = (
       <Typography sx={{ fontSize: 40 }} color="text.secondary" gutterBottom>
         Waiting Queue...
       </Typography>
+      <Typography>
+
+      </Typography>
     </CardContent>
     <CardActions>
       <Button variant="contained" disabled>
@@ -42,45 +45,11 @@ export default function WaitingRoom (){
   console.log('inside waiting room',appointment)
   console.log('queuesize', appointment.doctorDetails.doctorQueueSize)
   const queueToken = parseInt(appointment.doctorDetails.doctorQueueSize)+1;
-  const currentQueue = parseInt(appointment.doctorDetails.doctorCurrentQueueSize)+1;
+  const currentQueue = parseInt(appointment.doctorDetails.doctorCurrentQueueSize);
   const [queueSize, setQueueSize] = React.useState(queueToken);
   const [message, setMessage] = React.useState(currentQueue);
-  // const getAppointment = async () => {
+  const [waitingQueue, setWaitingQueue] = React.useState(currentQueue-queueToken);
 
-  //   // let apptid = parseInt(appt);
-  //   const getApptApi = `http://localhost:8083/api/patientDetails/getAppointmentById/`;
-  //   const myHeaders = new Headers();
-  //   myHeaders.append("Content-Type", "application/json");
-  //   const requestOptions = {
-  //     method: "POST",
-  //     headers: myHeaders,
-  //     body: "",
-  //     redirect: "follow",
-  //   };
-  //   await fetch(
-  //     getApptApi,
-  //     requestOptions
-  //   )
-  //     .then((response) => {
-  //       if(response.status === 200) {
-  //         response.json().then((e)=>{
-  //           console.log(e);
-  //           // setAppointmentData(e);
-  //           appointmentData = e;
-  //           queueSize = e.doctorDetails.doctorQueueSize;
-  //           console.log(appointmentData);
-  //           console.log("queue size is : ");
-  //           console.log(queueSize);
-  //           // console.log(JSON.stringify(appointmentData));
-  //           // console.log(appointmentData.doctorDetails.doctorQueueSize);
-  //           // setQueueSize(e.doctorDetails.doctorQueueSize);
-  //         });
-          
-  //         handledisconnect();
-  //       }
-  //     })
-  //     .catch((error) => console.log("error", error));
-  // };
   const handledisconnect = async () => {
     let api = "http://localhost:8083/api/patientDetails/onCallDisconnect";
     const myHeaders = new Headers();
@@ -111,10 +80,10 @@ export default function WaitingRoom (){
     console.log("message from server ");
     console.log(msg);
     setMessage(msg);
+    setWaitingQueue(msg-queueSize);
   }
   return (
     <Box sx={{ minWidth: 300 }}>
-      <Card variant="outlined">{card}</Card>
       <SockJsClient
             url={SOCKET_URL}
             topics={['/topic/message']}
@@ -123,10 +92,32 @@ export default function WaitingRoom (){
             onMessage={msg => onMessageReceived(msg)}
             debug={false}
           />
+      {/* <Card variant="outlined">{card}</Card> */}
+      <Card variant="outlined">
+      <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 40 }} color="text.secondary" gutterBottom>
+        Waiting Queue 
+      </Typography>
+      <Typography sx={{ fontSize: 100 }} color="#1a237e" >
+      {waitingQueue}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      {waitingQueue>0 && (<Button variant="contained" disabled>
+        Join Call
+      </Button>) 
+       }
+       {waitingQueue == 0 && (<Button variant="contained" color="#1a237e">
+        Join Call
+      </Button>)}
+    </CardActions>
+  </React.Fragment>
+  </Card>
           <Button onClick={handledisconnect}>Disconnect</Button>
       Message from socket = {message}<br/>
       Queue = {queueSize}<br/>
-      Waiting line = {message-queueSize}
+      Waiting line = {waitingQueue}
     </Box>
   );
 };
