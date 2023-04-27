04b24576edeb21e5eb9e50d4653e6e4907265b79
Parul <parulghotikar@gmail.com>
2023-04-28 01:08:42 +0530

    flow

--- frontend/user-patient/src/components/Home/Home.js
+++ frontend/user-patient/src/components/Home/Home.js
@@ -3,6 +3,7 @@ import "./Home.css";
 import Slides from "./Slides/Slides";
 import Header from "../Bar/Header/Header";
 import Footer from "../Bar/Footer";
+import Flow from "./Flow/Flow";
 const Home = () => {
   localStorage.removeItem("user");
   localStorage.removeItem("patient");
@@ -11,6 +12,7 @@ const Home = () => {
     <div id="home">
       <Header></Header>
       <Slides></Slides>
+      <Flow></Flow>
       <Footer></Footer>
     </div>
   );
